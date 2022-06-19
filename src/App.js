eb5be9e1ee1d82cdb2b5c6f91fcc2a0cc5d7fff1
juanilopezp <juanilopezpatterson@gmail.com>
2022-06-18 23:44:31 -0300

    cambio de carpetas
    termine de pasar el proyecto a react, ahora lo pongo en github

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <center>
+        <header>
+          <input type="search" name="busqueda"/>
+          <button class="enlace" role="link" onclick="window.location='pantalla_deporte.html'">buscar</button>
+        </header>
+         <div class="cajas_buscador">
+          <div class="deporte">
+            <a href="pantalla_deporte.html">Deportes</a>
+          </div>
+          <div class="musica">
+            <a href="pantalla_musica.html">Musica</a>
+          </div>
+          <div class="pais">
+            <a href="pantalla_pais.html">Pais</a>
+          </div>
+          <div class="estudio">
+             <a href="pantalla_estudio.html">Estudio</a>
+          </div> 
+        </div>
+        </center>
+    </div>
+  );
+}
+
+export default App;
