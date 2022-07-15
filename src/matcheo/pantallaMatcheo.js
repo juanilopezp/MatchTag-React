f2e3ed4b171483e6b0b7f0ae394140bbe5d41c73
juanilopezp <71837207+juanilopezp@users.noreply.github.com>
2022-07-15 12:15:07 -0300

    react link

--- src/matcheo/pantallaMatcheo.js
+++
@@ -1,9 +0,0 @@
-function pantallaMatcheo(){
-    let irPerfil =  document.getElementsByClassName("verPerfil");
-    irPerfil.addEventListener("click", irPerfil);
-    function yendoPerfil(){
-        alert("poronga");
-    }
-}
-
-export default pantallaMatcheo;
--- src/matcheo/pantallaMatcheo.jsx
+++ src/matcheo/pantallaMatcheo.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import img from './messiPer.jpg';
 import './pantallaMatcheo.css';
+import App from "../App";
 import pantallaMatcheo from "./pantallaMatcheo";
+import PantallaOtro from "./pantalla_otro";
+import {Route, Link} from "react-router-dom";
 function Matcheo(){
+    const yendoPerfil = () =>{
+            <Route path='pantalla_otro'/>
+    }
     return(
         <>
             <center>
                 <div class='fotoMatch'>
                     <img src={img} />
-                    <button class='verPerfil'>Perfil</button>
+                    <button class='verPerfil'>Perfil<Link to={'/pantalla_otro'}></Link></button>
                 </div>
                 <label class='nomUsuario'></label>
                 <button class='agregarM'></button>
-                <button class='chatM'></button>
                 <label class='tagSim'></label>
             </center>
- 
         </>          
+        /*react router navigate*/
     );
 }
 export default Matcheo;
