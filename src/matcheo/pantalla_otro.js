5fac54c2f9c83e116b3846d77505c8442072d200
juanilopezp <71837207+juanilopezp@users.noreply.github.com>
2022-08-08 17:19:25 -0300

    8/8 'chopija

--- src/matcheo/pantalla_otro.js
+++ src/matcheo/pantalla_otro.js
@@ -1,37 +1,36 @@
 import React from "react";
 import './pantalla_otro.css';
 import DataUsu from './Usuarios.json'
-function PantallaOtro(){
+import { useRef, useEffect } from 'react';
+function PantallaOtro() {
 	{/*let btnCambio = document.getElementById('btnPO');
 	btnCambio.addEventListener('click', () =>{
 		btnCambio.innerText = 'Esperando aprobacion';
 	})
 	*/ }
-	//let btnMatcheo = document.getElementById('btnMatch')
-	//btnMatcheo.addEventListener('click', function(btnMatcheo) {btnMatcheo.innerText = 'perando'})
-	return(
-	<>
-	{DataUsu.map(user =>{
-		return(
-		<div class="usuarioTop">
-				<h1>{user.nomUsuario}</h1>
-				<div> tags: {user.tags}</div>
-				<img src={user.phPerfil}/> 
-		</div>
-		)
-	})}
-			
-				<div class="botones">
-					<button id="btnMatch">poronga</button>
-				</div>
-		<ul class="usuarioDown">
+	return (
+		<>
+			{DataUsu.map(user => {
+				return (
+					<div class="usuarioLeft">
+						<h1>{user.nomUsuario}</h1>
+						<div class='tags'> tags: {user.tags}</div>
+						<img src={user.phPerfil} />
+						<div class="botones">
+							<button id="btnMatch" onClick={(e) => e.target.innerText = (e.target.innerText === "Siguiendo" ? "Seguir" : 'Siguiendo')}>Seguir</button>
+						</div>
+					</div>
+				)
+			})}
+
+
+			<ul class="usuarioRight">
 				<div class="publicaciones">
 					<slider>
 
 					</slider>
 				</div>
-		</ul></>
-
+			</ul></>
 	)
 }
 export default PantallaOtro
