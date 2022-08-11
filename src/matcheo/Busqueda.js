5b82b1bb5447fe65b2bcf60648c17215774447fe
juanilopezp <juanilopezpatterson@gmail.com>
2022-08-11 18:43:33 -0300

    pregym

--- src/matcheo/Busqueda.js
+++ src/matcheo/Busqueda.js
@@ -1,17 +1,30 @@
 import React from "react";
 import './Busqueda.css'
+import DataUsu from './Usuarios.json'
+import { useState } from 'react';
 function Busqueda(){
+    const[searchTerm, setSearchTerm] = useState('')
     return(
-        <center>
-            <section>
+        <>
+        
+            
                 <form autocomplete="off">
-                    <div class = "Buscador">
-                        <input type="text" name="busqueda" placeholder='Buscar Etiqueta' data-search/>
+                    <div id = 'busUsu' class = "Buscador">
+                        <input type="text" name="busqueda" placeholder='Buscar Etiqueta'onChange={event =>{setSearchTerm(event.target.value)}} data-search/>
           {/*<button class="enlace"> </button>*/}
                     </div>
                 </form>
-            </section>
-        </center>
+           
+        
+        {DataUsu.filter((val)=>{
+            if(searchTerm === ""){
+              return null
+            } else if(val.nomUsuario.toLowerCase().includes(searchTerm.toLowerCase())){
+              return val
+            }
+          }).map((val, key) =>{
+         return <button id='busUsu' class='Usuarios'>{val.nomUsuario}  </button>
+       })}</>
     );
 }
 export default Busqueda 
