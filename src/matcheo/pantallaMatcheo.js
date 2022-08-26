f84f864a88cc793c06cc66721a3385de3336b3dc
juanilopezp <71837207+juanilopezp@users.noreply.github.com>
2022-08-26 12:07:41 -0300

    AAAAAAAAAAAAAAAAAAAAAA

--- src/matcheo/pantallaMatcheo.js
+++ src/matcheo/pantallaMatcheo.js
@@ -6,50 +6,6 @@ import {Route, Link} from "react-router-dom";
 import DataUsu from './Usuarios.json';
 function Matcheo(){
     const [siguiendo, setSiguiendo] = useState(false);
-    const Cheshire = ({children}) => {
-        const slides = useMemo(() => {
-            if(children.lenght > 1){
-                let items = Children.map(children,(child, index) => (
-                    <li key={index}>
-                        {child}
-                    </li>
-                ))
-                return [
-                    <li key={children.lenght + 1}>
-                        {children[children.lenght - 1]}
-                    </li>,
-                    ...items,
-                    <li key={children.lenght + 2}>
-                        {children[0]}
-                    </li>
-                ];
-            }
-            return <li>{children[0]}</li>
-        }, [children])
-        return(
-            <>
-            <div class="top-matcheo">
-                            
-                                <div id = 'user-render' class='fotomatch-container'>
-                                    <div class='fotoMatch'>
-                                        {slides}
-                                        <Link class='hoverFotoMatch' to='/pantalla_otro'>
-                                            Perfil
-                                        </Link>
-                                    </div>
-                                    {slides}
-                                </div>
-                            <div class='agregarM-container'>
-                                <button class='agregarM' onClick={() => setSiguiendo(s => !s)}></button>
-                                <Link class='child-link' to='/pantalla_otro'></Link>
-                            </div>
-                        </div>
-                            <div id = 'user-render'class='labels-container'>
-                                {slides}
-                            </div>
-                            </>
-        )
-    }
     return(
         <>
             
@@ -64,26 +20,36 @@ function Matcheo(){
                 */}
                 
                     {DataUsu.map((a, index)=>{
-                        return( 
+                        return(
                         <>
-                        <Cheshire>
-                            <img src={a.phPerfil} />
-                            <label class='nomUsuario'>{a.nomUsuario}</label>
-                            <label class='tagSim'>
+                        <div class='CheshireCat'>
+                            <div class="top-matcheo">
+                                <div id = 'user-render' class='fotomatch-container'>
+                                    <div class='fotoMatch'>
+                                        <img src={a.phPerfil} />
+                                        <Link class='hoverFotoMatch' to='/pantalla_otro'>
+                                            Perfil
+                                        </Link>
+                                    </div>
+                                    <label class='nomUsuario'>{a.nomUsuario}</label>
+                                </div>
+                            <div class='agregarM-container'>
+                                <button class='agregarM' onClick={() => setSiguiendo(s => !s)}></button>
+                                <Link class='child-link' to='/pantalla_otro'></Link>
+                            </div>
+                            </div>
+                            <div id = 'user-render'class='labels-container'>
+                                <label class='tagSim'>
                                     {a.tags}
-                            </label>
-                        </Cheshire>
+                                </label>
+                            </div>
+                        </div>
+                            <button class ='btnIzq'></button>
+                            <button class='btnDer'></button>  
                             </>
                             )
                     })}
-                
-                <button class ='btnIzq'>
-                </button>
-                <button class='btnDer'>
-                </button>
- 
-        </>          
-        /*react router navigate*/
+                    </>
     );
 }
 export default Matcheo;
