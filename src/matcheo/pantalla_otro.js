3e6c5c275bf708f251dc5d4296f816b70aa38d5c
juanilopezp <juanilopezpatterson@gmail.com>
2022-09-29 23:00:14 -0300

    cacho01

--- src/matcheo/pantalla_otro.js
+++ src/matcheo/pantalla_otro.js
@@ -10,67 +10,75 @@ function PantallaOtro() {
 	})
 	*/ }
 	const [siguiendo, setSiguiendo] = useState(false);
+	const [like, setLike] = useState(false);
+	function likeFunction(){
+		if(like){
+			setLike(false)
+		}
+		else{
+			setLike(true)
+		}
+	}
 	const [comentarios, setComentarios] = useState('')
 	const [mostrarCom, setMostrarCom] = useState([])
 	const onClickHandler = () =>{
 		setMostrarCom((mostrarCom)=>[...mostrarCom, comentarios]).then(()=>{
-			$("#output").val("");
+			("#output").val("");
 		});
 	}
-	
 	const onChangeHandler = (e) =>{
 		setComentarios (e.target.value)
 	}
 	return (
-
-
-		<>
-
-			{DataUsu.map(user => {
-				return (
-					<>
-						<div class='container'>
-							<div class="usuarioLeft">
-								<img class='phPerfil' src={user.phPerfil} />
-								<h1>{user.nomUsuario}</h1>
-								<div class="botones">
-									<button 
-									id="btnMatch" 
-									onClick={() => setSiguiendo(s => !s)}>{siguiendo ? "Siguiendo" : "Seguir"}
+	<>
+		{DataUsu.map(user => {
+			return (
+			<>
+				<div class='container'>
+					<div class="usuarioLeft">
+						<img class='phPerfil' src={user.phPerfil} />
+						<h1>{user.nomUsuario}</h1>
+						<div class="botones">
+							<button 
+							id="btnMatch" 
+							onClick={() => setSiguiendo(s => !s)}>{siguiendo ? "Siguiendo" : "Seguir"}
+							</button>
+						</div>
+						<div class='tags-container'>
+							<div class='tags'>{user.tags}</div>
+						</div>
+					</div>
+					<div class="usuarioRight">
+						<h1>Publicaciones</h1>
+						<motion.div class="publicaciones-container" drag='y'>
+							<div class='publicaciones'>
+								<img src={user.publicaciones} />
+								<div 
+								className={[like ? 'active-like': 'like']}
+								onClick={likeFunction}>
+								</div>
+							</div>
+							<div class='comentarios-container'>
+								{mostrarCom.map((text)=>(
+									<div class='comentarios'>
+										{text}
+									</div>
+								))}
+								<div class='input-container'>
+									<input 
+									value={comentarios} 
+									onChange={onChangeHandler} 
+									class='caja-input'
+									/>
+									<button onClick={onClickHandler} class='subir-btn'>
+										Subir
 									</button>
 								</div>
-								<div class='tags-container'>
-									<div class='tags'>{user.tags}</div>
-								</div>
 							</div>
-							<div class="usuarioRight">
-								<h1>Publicaciones</h1>
-								<motion.div class="publicaciones-container" drag='y'>
-									<div>
-										<img class='publicaciones' src={user.publicaciones} />
-										<div class='likes'></div>
-									</div>
-									<div class='comentarios-container'>
-										{mostrarCom.map((text)=>(
-											<div class='comentarios'>
-												{text}
-											</div>
-										))}
-										<div class='input-container'>
-											<input 
-											value={comentarios} 
-											onChange={onChangeHandler} 
-											class='caja-input'
-											/>
-											<button onClick={onClickHandler} class='subir-btn'>
-												Subir
-											</button>
-										</div>
-										
-									</div>
-								</motion.div>
-							</div>
-						</div></>
+						</motion.div>
+					</div>
+				</div>
+			</>
 				)
 			})}
 		</>
